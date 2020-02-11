e555876c02f55899c2795c11520f35fa23eacceb
hughwin <hugh.winchester@gmail.com>
2020-02-11 20:18:52 +0000

    Completed Parameterised URL Mapping

--- populate_rango.py
+++ populate_rango.py
@@ -11,7 +11,7 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [{'title': 'Official Python Tutorial', 'url': 'http://docs.python.org/3/tutorial/'},
-                    {'title': 'How to Think Like a Computer Scientist',
+                    {'title': 'How to Think like a Computer Scientist',
                      'url': 'http://www.greenteapress.com/thinkpython/'},
                     {'title': 'Learn Python in 10 Minutes',
                      'url': 'http://www.korokithakis.net/tutorials/python/'}]
