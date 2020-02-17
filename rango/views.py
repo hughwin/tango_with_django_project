e584aab9386bc65591a142b250c55b60ea165fc7
hughwin <hugh.winchester@gmail.com>
2020-02-17 17:34:38 +0000

    All tests passed

--- rango/views.py
+++ rango/views.py
@@ -23,14 +23,17 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['visits'] = int(request.COOKIES.get('visits', 1))
+
+    visitor_cookie_handler(request)
     response = render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request, response)
     return response
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
@@ -48,13 +51,13 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/add_category.html', context=context_dict)
 
+
 @login_required
 def add_category(request):
     form = CategoryForm()
 
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-
 
         if form.is_valid():
 
@@ -65,13 +68,13 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+
 @login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
-
 
     if category is None:
         return redirect('/rango/')
@@ -132,6 +135,7 @@ def register(request):
                            'profile_form': profile_form,
                            'registered': registered})
 
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -152,20 +156,32 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', 1))
+
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
 
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
 
-    response.set_cookie('visits', visits)
+    request.session['visits'] = visits
+
 
 @login_required
 def restricted(request):
